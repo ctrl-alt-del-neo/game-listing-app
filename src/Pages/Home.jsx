66c716c1facee5341e04ec9a0c40d59f4c040e1f
gaby <gabriel@xmanna.com>
2023-12-22 00:18:43 +0200

    Trending Games section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react"
 import GenreList from "../Components/GenreList"
 import GlobalAPI from "../Services/GlobalAPI"
 import Banner from "../Components/Banner";
+import TrendingGames from "../Components/TrendingGames";
 
 function Home() {
-    const [allGameList, setAllGanmeList]=useState();
+    const [allGameList, setAllGameList]=useState();
     useEffect(()=>{
         getAllGamesList();
     },[])
     const getAllGamesList=()=>{
         GlobalAPI.getAllGames.then((res)=>{
-            setAllGanmeList(res.data.results);
+            setAllGameList(res.data.results);
         })
     }
   return (
@@ -19,7 +20,11 @@ function Home() {
             <GenreList/>
         </div>
         <div className="col-span-4 md:col-span-3">
-            {allGameList?.length>0 ?<Banner gameBanner={allGameList[0]}/>
+            {allGameList?.length>0 ?
+            <div>
+                <Banner gameBanner={allGameList[0]}/>
+                <TrendingGames gameList={allGameList}/>
+            </div>
             :null}
         </div>
 
