80d433563a07366364718d478abf4f6a0b928291
gaby <gabriel@xmanna.com>
2023-12-21 19:27:50 +0200

    Genre styling

--- src/Components/GenreList.jsx
+++ src/Components/GenreList.jsx
@@ -1,17 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import GlobalAPI from '../Services/GlobalAPI'
 
 function GenreList() {
+    const [genreList, setGenreList]=useState([]);
+    const [activeIndex, setActiveIndex]=useState(0);
     useEffect(()=>{
         getGenereList();
     })
     const getGenereList=()=>{
         GlobalAPI.getGenereList.then((res)=>{
             console.log(res.data.results);
+            setGenreList(res.data.results);
         })
     }
   return (
-    <div>GenreList</div>
+    <div>
+        <h2 className='text-[30px] font-bold dark:text-white'>Genre</h2>
+        {genreList.map((item, index)=>(
+            <div onClick={()=>setActiveIndex(index)}
+            className={`flex gap-2 items-center mb-2 cursor-pointer 
+            hover:bg-gray-300 p-2 group rounded-lg 
+            hover:dark:bg-gray-600
+           ${activeIndex==index?"bg-gray-300 dark:bg-gray-600": null}`}>
+                <img src={item.image_background} 
+                className={`w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105
+                transition-all ease-out duration-300
+                ${activeIndex==index?"scale-105": null}`}/>
+                <h3 className={`dark:text-white text-[18px] group-hover:font-bold
+                transition-all ease-out duration-300'
+                ${activeIndex==index?"font-bold": null}`}>{item.name}</h3>
+            </div>
+        ))}
+    </div>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,10 +3,11 @@ import GenreList from "../Components/GenreList"
 function Home() {
   return (
     <div className="grid grid-cols-4">
-        <div className="bg-red-600 h-full hidden md:block">
+        <div className="px-5 hidden md:block">
             <GenreList/>
         </div>
         <div className="col-span-4 md:col-span-3 bg-blue-400">GameList</div>
+
     </div>
   )
 }
